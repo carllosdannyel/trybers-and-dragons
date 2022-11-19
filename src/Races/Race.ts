048697849215c4398b54e17dc4f417e3013148c5
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-11-19 04:50:49 -0300

    cria a classe Race e exporta como default pelo index

---
+++ src/Races/Race.ts
@@ -0,0 +1,17 @@
+abstract class Race {
+  private readonly _name: string; private readonly _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name; this._dexterity = dexterity;
+  }
+
+  get name(): string { return this._name; }
+
+  get dexterity(): number { return this._dexterity; }
+
+  static createdRacesInstances(): number { throw new Error('Not implemented'); }
+  
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
