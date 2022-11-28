a2f8da3466feefaeb314abf36fb195fd105b9625
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-11-28 14:56:35 -0300

    cria a classe Archetype

---
+++ src/Archetypes/Archetypes.ts
@@ -0,0 +1,32 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private readonly _name: string;
+  private readonly _special: number;
+  private readonly _cost: number;
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not Implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
